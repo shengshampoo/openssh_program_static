d055a8b54eac5429fafeeffc9360bf1d4ce97eca
shengshampoo <shengzh@tutanota.com>
2025-10-06 14:14:42 +0000

    Create build-static-openssh.sh

---
+++ build-static-openssh.sh
@@ -0,0 +1,22 @@
+
+#! /bin/bash
+
+set -e
+
+WORKSPACE=/tmp/workspace
+mkdir -p $WORKSPACE
+mkdir -p /work/artifact
+
+# openssh
+cd $WORKSPACE
+hh=10.1p1
+curl -s https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-$hh.tar.gz | tar x --gzip
+cd openssh-$hh
+LDFLAGS="-static -no-pie -s" ./configure --prefix=/usr/local/opensshmm --sysconfdir=/usr/local/opensshmm/etc/ssh --without-pam --with-privsep-path=/usr/local/opensshmm/lib/sshd/ --with-pid-dir=/usr/local/opensshmm/run --with-mantype=man
+make
+make install
+
+cd /usr/local
+tar vcJf ./opensshmm.tar.xz opensshmm
+
+mv ./opensshmm.tar.xz /work/artifact/
