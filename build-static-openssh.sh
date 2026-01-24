2874b7df1b20cf07a0bec27419d6be12f113ac9e
shengshampoo <shengzh@tutanota.com>
2026-01-24 18:07:27 +0000

    Update build-static-openssh.sh

--- build-static-openssh.sh
+++ build-static-openssh.sh
@@ -9,7 +9,7 @@ mkdir -p /work/artifact
 
 # openssh
 cd $WORKSPACE
-hh=10.2p1
+hh=$(curl -s https://www.openssh.org/releasenotes.html | grep -Po '\K[0-9.]{4}p1+' | head -n 1)
 curl -s https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-$hh.tar.gz | tar x --gzip
 cd openssh-$hh
 ./configure --prefix=/usr/local/opensshmm --sysconfdir=/usr/local/opensshmm/etc/ssh --without-pam --with-privsep-path=/usr/local/opensshmm/lib/sshd/ --with-pid-dir=/usr/local/opensshmm/run --with-mantype=man --with-libedit --with-ldns
